eaa14cd9bfb65bb1d49e48df3707a0db2db6cc30
Corbin Matamoros <35641464+Pirhomega@users.noreply.github.com>
2018-03-24 11:13:10 -0500

    Create Source.cpp

---
+++ Assignments/toUpper_function/Source.cpp
@@ -0,0 +1,37 @@
+```cpp
+#include <string>
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+string toUpper(string lower)
+{
+	for (int i = 0; i < lower.length(); i++)
+	{
+		if ((int)lower[i] >= 97 && (int)lower[i] <= 122)
+		{
+			lower[i] = lower[i] - 32;
+		}
+	}
+	return lower;
+}
+
+int main()
+{
+	string upperWord = " ", lowerWord = " ";
+	ifstream infile;
+	ofstream outfile;
+	infile.open("Input.txt");
+	outfile.open("Output.txt");
+	cout << lowerWord << '\n';
+	while (infile >> lowerWord)
+	{
+		upperWord = toUpper(lowerWord);
+		outfile << upperWord << '\n';
+	}
+	outfile.close();
+	infile.close();
+	return 0;
+}
+```
